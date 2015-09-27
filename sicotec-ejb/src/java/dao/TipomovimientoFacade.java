28f324db7ccd716ef297c1f64e1972964540e93c
rikardo308 <rikardo308@gmail.com>
2015-09-26 23:28:06 -0500

    Ricardo

--- sicotec-ejb/src/java/dao/TipomovimientoFacade.java
+++ sicotec-ejb/src/java/dao/TipomovimientoFacade.java
@@ -6,9 +6,12 @@
 package dao;
 
 import entidades.Tipomovimiento;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -26,6 +29,25 @@ public class TipomovimientoFacade extends AbstractFacade<Tipomovimiento> {
 
     public TipomovimientoFacade() {
         super(Tipomovimiento.class);
+    }
+    
+    public List<Tipomovimiento> getAllTMovimiento(){
+        List<Tipomovimiento> tMovimiento = new ArrayList<Tipomovimiento>();
+        
+        
+        try{
+            String jpa = "SELECT t "
+                       + "FROM Tipomovimiento t";
+
+            Query query = em.createQuery(jpa,Tipomovimiento.class);
+            tMovimiento = query.getResultList();
+            
+        }catch(Exception e){
+            tMovimiento = new ArrayList<Tipomovimiento>();
+        }
+        
+        
+        return tMovimiento;
     }
     
 }
